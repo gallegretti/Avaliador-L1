dce045b124f130d2f9dca868b0adbb68233b4cf3
gallegretti <gabriel13082@hotmail.com>
2018-06-12 21:55:07 -0300

    Lists and examples

--- src/app.kt
+++ src/app.kt
@@ -1,3 +1,4 @@
+
 open class Expression
 
 class ExpNumber(val n: Int) : Expression() {
@@ -27,9 +28,19 @@ class ExpNotEqual(e1: Expression, e2: Expression) : ExpLogicOp(e1, e2)
 class ExpLessOrEqual(e1: Expression, e2: Expression) : ExpLogicOp(e1, e2)
 class ExpLess(e1: Expression, e2: Expression) : ExpLogicOp(e1, e2)
 
+// Lists
+
+class ExpNil() : Expression()
+class ExpList(val list: MutableList<Expression>) : Expression()
+class ExpConcat(val e1: Expression, val e2: Expression) : Expression()
+class ExpHead(val e1: Expression) : Expression()
+class ExpTail(val e1: Expression) : Expression()
+class ExpIsEmpty(val e1: Expression) : Expression()
+
 
 fun ExpEval(exp: Expression): Expression {
     return when (exp) {
+
     // Is value
         is ExpBool -> exp
         is ExpNumber -> exp
@@ -70,7 +81,54 @@ fun ExpEval(exp: Expression): Expression {
         is ExpLess -> {
             auxDoLogicOp(exp, { a, b -> a < b }, "<")
         }
-        else -> exp
+
+    // Lists
+        is ExpNil -> ExpList(arrayListOf())
+        is ExpList -> exp
+        is ExpConcat -> {
+            val lhs = ExpEval(exp.e1)
+            val rhs = ExpEval(exp.e2)
+            if (rhs is ExpList) {
+                rhs.list.add(lhs)
+                return ExpList(rhs.list)
+            } else {
+                throw Exception("The operation 'Concat' was performed on a non-list expression")
+            }
+        }
+        is ExpHead -> {
+            val l = ExpEval(exp.e1)
+            if (l is ExpList) {
+                if (l.list.isNotEmpty()) {
+                    return ExpEval(l.list[0])
+                } else {
+                    throw Exception("The operation 'Head' was performed on an empty list")
+                }
+            } else {
+                throw Exception("The operation 'Head' was performed on a non-list expression")
+            }
+        }
+        is ExpTail -> {
+            val l = ExpEval(exp.e1)
+            if (l is ExpList) {
+                if (l.list.isNotEmpty()) {
+                    return ExpList(l.list.drop(0).toMutableList())
+                } else {
+                    throw Exception("The operation 'Tail' was performed on an empty list")
+                }
+            } else {
+                throw Exception("The operation 'Tail' was performed on a non-list expression")
+            }
+        }
+        is ExpIsEmpty -> {
+            val l = ExpEval(exp.e1)
+            if (l is ExpList) {
+                return ExpBool(l.list.isEmpty())
+            } else {
+                throw Exception("The operation 'IsEmpty' was performed on a non-list expression")
+            }
+        }
+
+        else -> throw Exception("Unimplemented expression type ${exp.javaClass.name}")
     }
 }
 
@@ -99,6 +157,8 @@ fun main(args: Array<String>) {
 
 
     // Examples:
+
+    // (10+10) == 20
     println(
             ExpEval(
                     ExpEqual(
@@ -110,12 +170,44 @@ fun main(args: Array<String>) {
                     )
             )
     )
+
+    // if (2 < 0) then 42 else 0
     println(
             ExpEval(
                     ExpIf(
                             ExpLess(ExpNumber(2), ExpNumber(0)),
                             ExpNumber(42),
                             ExpNumber(0)
+                    )
+            )
+    )
+
+    // isEmpty([nil])
+    println(
+            ExpEval(
+                    ExpIsEmpty(ExpNil())
+            )
+    )
+
+    // isEmpty([0::nil])
+    println(
+            ExpEval(
+                    ExpIsEmpty(
+                            ExpConcat(
+                                    ExpNumber(0),
+                                    ExpNil()
+                            )
+                    )
+            )
+    )
+
+    // hd(tl([0::(1::nil)]))
+    println(
+            ExpEval(
+                    ExpHead(
+                            ExpTail(
+                                    ExpConcat(ExpNumber(0), ExpConcat(ExpNumber(1), ExpNil()))
+                            )
                     )
             )
     )
