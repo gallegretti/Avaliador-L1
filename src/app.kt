d8b28808a0bfff8286b8c8fc12b97ac5275003aa
gallegretti <gabriel13082@hotmail.com>
2018-06-22 22:13:42 -0300

    Added enviroment to expressions, added tests and fixed recursive functions

--- src/app.kt
+++ src/app.kt
@@ -1,18 +1,28 @@
-open class Expression
+// Types
+
+open class Type()
+
+class TyInt : Type()
+class TyBool : Type()
+class TyFn(val input: Type, val output: Type)
+
+
+open class Expression(var environment: MutableMap<String, Expression> = mutableMapOf())
 
 // Values
 
-class ExpNumber(val n: Int) : Expression() {
+data class ExpNumber(val n: Int) : Expression() {
     override fun toString(): String {
         return n.toString()
     }
 }
 
-class ExpBool(val b: Boolean) : Expression() {
+data class ExpBool(val b: Boolean) : Expression() {
     override fun toString(): String {
         return b.toString()
     }
 }
+
 
 // Conditional
 
@@ -36,11 +46,12 @@ class ExpLessOrEqual(e1: Expression, e2: Expression) : ExpLogicOp(e1, e2)
 class ExpLess(e1: Expression, e2: Expression) : ExpLogicOp(e1, e2)
 
 // Functions
+class ExpVar(val variable: String) : Expression()
 
 class ExpApply(val e1: Expression, val e2: Expression) : Expression()
-class ExpLambda(val function: (Expression) -> Expression) : Expression()
-class ExpFunction(val function: Expression, val scope: (Expression) -> Expression) : Expression()
-
+class ExpLambda(val parameterName: String, val body: Expression) : Expression()
+class ExpFunction(val functionName: String, val parameterName: String, val body: Expression, val scope: Expression) : Expression()
+class ExpFunctionRec(val functionName: String, val parameterName: String, val body: Expression, val scope: Expression) : Expression()
 // Lists
 
 class ExpNil() : Expression() {
@@ -58,7 +69,7 @@ class ExpIsEmpty(val e1: Expression) : Expression()
 // Exceptions
 
 class ExpTry(val _try: Expression, val with: Expression) : Expression()
-class ExpRaise() : Expression() {
+data class ExpRaise(val dummy: Unit? = null) : Expression() {
     override fun toString(): String {
         return "raise"
     }
@@ -67,26 +78,49 @@ class ExpRaise() : Expression() {
 
 fun ExpEval(exp: Expression): Expression {
     return when (exp) {
-
+        is ExpVar -> {
+            return exp.environment[exp.variable] ?: ExpRaise()
+        }
     // Functions
         is ExpLambda -> exp
         is ExpApply -> {
-            val lambda = ExpEval(exp.e1)
-            if (lambda is ExpLambda) {
-                val functionArgument = ExpEval(exp.e2)
-                return ExpEval(lambda.function(functionArgument))
-            } else {
-                throw Exception("The operation 'Apply' was performed on a non-lambda expression")
+            exp.e1.environment = exp.environment
+            exp.e2.environment = exp.environment
+            val function = ExpEval(exp.e1)
+            if (function is ExpFunctionRec) {
+                function.body.environment = exp.environment
+                function.environment[function.parameterName] = ExpEval(exp.e2)
+                return ExpEval(function.body)
+            }/*
+            if (function is ExpLambda) {
+                function.body.environment = exp.environment
+                function.environment[function.parameterName] = ExpEval(exp.e2)
+                return ExpEval(function.body)
+            }*/
+            if (function is ExpFunction) {
+                function.body.environment = exp.environment
+                function.body.environment[function.parameterName] = ExpEval(exp.e2)
+                return ExpEval(function.body)
             }
+
+            return ExpRaise()
+
         }
         is ExpFunction -> {
-            val function = ExpEval(exp.function)
-            return ExpEval(exp.scope(function))
+            exp.scope.environment = exp.environment
+            exp.scope.environment[exp.functionName] = exp
+            return ExpEval(exp.scope)
         }
-
+        is ExpFunctionRec -> {
+            exp.scope.environment = exp.environment
+            exp.scope.environment[exp.functionName] = exp
+            return ExpEval(exp.scope)
+        }
 
     // Exceptions
         is ExpTry -> {
+            exp._try.environment = exp.environment
+            exp.with.environment = exp.environment
             val block = ExpEval(exp._try)
             return when (block) {
                 is ExpRaise -> ExpEval(exp.with)
@@ -101,9 +135,13 @@ fun ExpEval(exp: Expression): Expression {
         is ExpNumber -> exp
     // If
         is ExpIf -> {
+            exp.e1.environment = exp.environment
+            exp.e2.environment = exp.environment
+            exp.e3.environment = exp.environment
             val condition = ExpEval(exp.e1)
-            when (condition) {
-                is ExpBool -> return if (condition.b) ExpEval(exp.e2) else ExpEval(exp.e3)
+            return when (condition) {
+                is ExpBool -> if (condition.b) ExpEval(exp.e2) else ExpEval(exp.e3)
+                is ExpRaise -> ExpRaise()
                 else -> throw Exception("Condition is not a boolean value")
             }
         }
@@ -141,6 +179,8 @@ fun ExpEval(exp: Expression): Expression {
         is ExpNil -> ExpList(arrayListOf())
         is ExpList -> exp
         is ExpConcat -> {
+            exp.e1.environment = exp.environment
+            exp.e2.environment = exp.environment
             val lhs = ExpEval(exp.e1)
             val rhs = ExpEval(exp.e2)
             if (rhs is ExpList) {
@@ -151,6 +191,7 @@ fun ExpEval(exp: Expression): Expression {
             }
         }
         is ExpHead -> {
+            exp.e1.environment = exp.environment
             val l = ExpEval(exp.e1)
             when (l) {
                 is ExpList -> return if (l.list.isNotEmpty()) {
@@ -164,6 +205,7 @@ fun ExpEval(exp: Expression): Expression {
 
         }
         is ExpTail -> {
+            exp.e1.environment = exp.environment
             val l = ExpEval(exp.e1)
             when (l) {
                 is ExpList -> return if (l.list.isNotEmpty()) {
@@ -176,6 +218,7 @@ fun ExpEval(exp: Expression): Expression {
             }
         }
         is ExpIsEmpty -> {
+            exp.e1.environment = exp.environment
             val l = ExpEval(exp.e1)
             if (l is ExpList) {
                 return ExpBool(l.list.isEmpty())
@@ -189,131 +232,147 @@ fun ExpEval(exp: Expression): Expression {
 }
 
 fun auxDoArithmeticOp(exp: ExpArithmeticOp, f: (Int, Int) -> Int, name: String): Expression {
+    exp.e1.environment = exp.environment
+    exp.e2.environment = exp.environment
     val lhs = ExpEval(exp.e1)
     val rhs = ExpEval(exp.e2)
-    if (lhs is ExpNumber && rhs is ExpNumber) {
-        return ExpNumber(f(lhs.n, rhs.n))
+    return if (lhs is ExpNumber && rhs is ExpNumber) {
+        ExpNumber(f(lhs.n, rhs.n))
     } else if (lhs is ExpRaise || rhs is ExpRaise) {
-        return ExpRaise()
+        ExpRaise()
     } else {
         throw Exception("The operation '$name' was performed on non-numeric expressions")
     }
 }
 
 fun auxDoLogicOp(exp: ExpLogicOp, f: (Int, Int) -> Boolean, name: String): Expression {
+    exp.e1.environment = exp.environment
+    exp.e2.environment = exp.environment
     val lhs = ExpEval(exp.e1)
     val rhs = ExpEval(exp.e2)
-    if (lhs is ExpNumber && rhs is ExpNumber) {
-        return ExpBool(f(lhs.n, rhs.n))
+    return if (lhs is ExpNumber && rhs is ExpNumber) {
+        ExpBool(f(lhs.n, rhs.n))
     } else if (lhs is ExpRaise || rhs is ExpRaise) {
-        return ExpRaise()
+        ExpRaise()
     } else {
         throw Exception("The operation '$name' was performed on non-numeric expressions")
     }
 }
 
+fun assert(expression: Expression, expected: Expression) {
+    val evaluated = ExpEval(expression)
+    if (evaluated != expected) {
+        println("Test failed, expected $expected but found $evaluated")
+    } else {
+        println("Test succeeded")
+    }
+}
 
 fun main(args: Array<String>) {
 
     // Examples:
     // (10+10) == 20
-    println(ExpEval(
+    assert(
             ExpEqual(
                     ExpOpAdd(
                             ExpNumber(10),
                             ExpNumber(10)
                     ),
                     ExpNumber(20)
-            )
-    ))
+            ),
+            ExpBool(true)
+    )
 
     // if (2 < 0) then 42 else 0
-    println(ExpEval(
+    assert(
             ExpIf(
                     ExpLess(ExpNumber(2), ExpNumber(0)),
                     ExpNumber(42),
                     ExpNumber(0)
-            )
-    ))
+            ),
+            ExpNumber(0)
+    )
 
     // isEmpty([nil])
-    println(ExpEval(
-            ExpIsEmpty(ExpNil())
-    ))
+    assert(
+            ExpIsEmpty(ExpNil()), ExpBool(true)
+    )
 
     // isEmpty([0::nil])
-    println(ExpEval(
+    assert(
             ExpIsEmpty(
                     ExpConcat(
                             ExpNumber(0),
                             ExpNil()
                     )
-            )
-    ))
+            ), ExpBool(false)
+    )
 
     // hd(tl([0::(1::nil)]))
-    println(ExpEval(
+    assert(
             ExpHead(
                     ExpTail(
                             ExpConcat(ExpNumber(0), ExpConcat(ExpNumber(1), ExpNil()))
                     )
-            )
-    ))
+            ), ExpNumber(1)
+    )
 
     // try raise with true
-    println(ExpEval(
-            ExpTry(ExpRaise(), ExpBool(true))
-    ))
+    assert(
+            ExpTry(ExpRaise(), ExpBool(true)), ExpBool(true)
+    )
 
     // try (if (true) then (raise) else (false)) with false
-    println(ExpEval(
+    assert(
             ExpTry(
                     ExpIf(
                             ExpBool(true),
                             ExpRaise(),
                             ExpBool(false)
                     ),
-                    ExpBool(true)
-            )
-    ))
+                    ExpBool(false)
+            ), ExpBool(false)
+    )
 
     // try (if(true) then (hd(nil)) else nil) with 0
-    println(ExpEval(
+    assert(
             ExpTry(
                     ExpIf(ExpBool(true), ExpHead(ExpNil()), ExpNil()),
                     ExpNumber(0)
-            )
-    ))
+            ), ExpNumber(0)
+    )
 
     // 1 + hd(hd(1::nil))
-    println(ExpEval(
-            ExpOpAdd(ExpNumber(1), ExpTail(ExpTail(ExpConcat(ExpNumber(1), ExpNil()))))
-    ))
+    assert(
+            ExpOpAdd(ExpNumber(1), ExpTail(ExpTail(ExpConcat(ExpNumber(1), ExpNil())))),
+            ExpRaise()
+    )
 
     // (fn x : Int -> x + 1)(10)
-    println(ExpEval(
+    assert(
             ExpApply(
-                    ExpLambda({ x ->
-                        ExpOpAdd(
-                                ExpNumber(1),
-                                x
-                        )
-                    }), ExpNumber(1))
-    ))
+                    ExpLambda("x",
+                            ExpOpAdd(
+                                    ExpNumber(1),
+                                    ExpVar("x")
+                            )), ExpNumber(10)),
+            ExpNumber(11))
 
     // let inc = (fn x -> x + 1) in inc(1)
-    println(ExpEval(
-            ExpFunction(
-                    // let function
-                    ExpLambda({ x ->
-                        ExpOpAdd(
-                                ExpNumber(1),
-                                x
-                        )
-                    }),
-                    // 'inc' in ...
-                    ({ inc ->
-                        ExpApply(inc, ExpNumber(1))
-                    }))
-    ))
+    assert(
+            ExpFunction("inc", "x", ExpOpAdd(
+                    ExpNumber(1),
+                    ExpVar("x")
+            ),
+                    ExpApply(ExpVar("inc"), ExpNumber(1))
+            ), ExpNumber(2))
+
+
+    // let fat = (fn x -> if (x == 0) then 1 else x*fat(x-1) in fat(4)
+    assert(
+            ExpFunctionRec("fat", "x", ExpIf(ExpEqual(ExpVar("x"), ExpNumber(0)),
+                    ExpNumber(1),
+                    ExpOpMult(ExpVar("x"), ExpApply(ExpVar("fat"), ExpOpSub(ExpVar("x"), ExpNumber(1))))), ExpApply(ExpVar("fat"), ExpNumber(4))
+            ), ExpNumber(24)
+    )
 }
